29e8431f3c8370476e3b86c651817e9d28997c49
VladNeres <vlade02@hotmail.com>
2022-09-25 21:56:11 -0300

    feat: Criando crud-mss, criando rotas para usuarios

--- crud-mss/index.js
+++ crud-mss/index.js
@@ -1,21 +1,60 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
-
 app.use(express.json())
-app.listen(4000, () => {
-    console.log("servidor subiu...")
-})
+contador = 0;
 
 const usuarios = [];
 
-app.post("/usuarios", (req, res) => {
+app.post("/usuarios", (req, res) => { 
     const {
-        Nome,
-        CPF,
-        Email,
-        Senha,
-        TipoAcesso
+        nome,
+        cpf,
+        email,
+        senha,
+        tipoAcesso
     } = req.body;
-    usuarios.push({Nome,CPF,Email,senha,TipoAcesso})
+    usuarios.push({ nome, cpf, email, senha, tipoAcesso })
+    res.status(201).json(usuarios[contador]);
+    contador++;
+})
 
+app.put('/usuarios', (req, res) => {
+    const {
+        nome,
+        cpf,
+        email,
+        senha,
+        tipoAcesso
+    } = req.body;
+    const user = usuarios.find((user)=>{return user.cpf==cpf})
+   console.log(user)
+    user.nome= nome
+    user.cpf= cpf
+    user.email= email
+    user.senha=senha
+    user.tipoAcesso= tipoAcesso 
+    res.status(204).json(user);
+   
+})
+
+app.get("/usuarios", (req, res) => {
+   
+    const{email, senha} = req.query
+    const user = usuarios.find(user=>{return user.email == email && user.senha==senha
+    })
+    res.status(200).json(user)
+    
+})
+
+app.delete("/usuarios", (req,res)=>{
+
+    const cpf = req.body.cpf
+    usuarios.splice(usuarios.find((user,i)=>{if (user.cpf==cpf) return i}),1)
+    res.status(204).end()
+})
+
+
+app.listen(4000, () => {
+    console.log("servidor subiu...")
 })
