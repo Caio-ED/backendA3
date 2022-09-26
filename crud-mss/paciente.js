142955f6cbe6d5e32e348ced4472acf0fd9d0e34
catarinamanzieri <catarina.manzieri@gmail.com>
2022-09-26 20:56:32 -0300

    feat: crud-mss - atualizar o crud de paciente

--- crud-mss/paciente.js
+++ crud-mss/paciente.js
@@ -4,10 +4,6 @@ const router = express.Router();
 contador = 0;
 const pacientes = [];
 
-router.get('/pacientes', (req, res) => {
-    res.status(200).json(pacientes);
-})
-
 
 router.post('/cadastro', (req, res) => {
     const {
@@ -23,12 +19,37 @@ router.post('/cadastro', (req, res) => {
     contador ++;
 })
 
-// router.put('/user', (req, res) => {
-//     res.send('Got a PUT request at /user')
+router.put('/alterarCadastro', (req, res) => {
+    const {
+        nome,
+        dataNascimento,
+        cpf,
+        telefone,
+        entradaClinica,
+        endereco
+    } = req.body;
+    const paciente = pacientes.find((paciente)=>{return paciente.cpf==cpf})
+    console.log(paciente)
+    paciente.nome= nome
+    paciente.dataNascimento= dataNascimento
+    paciente.cpf= cpf
+    paciente.telefone= telefone
+    paciente.entradaClinica= entradaClinica
+    paciente.endereco= endereco
+    res.status(204).json(paciente);
+})
+
+// router.get("", (req, res) => {
+//     const cpf = req.query
+//     const paciente = pacientes.find(pac =>{ if (pac.cpf == cpf) return pac})
+//     res.status(200).json(paciente)
 // })
 
-// router.delete('/user', (req, res) => {
-//     res.send('Got a DELETE request at /user')
+
+// router.delete('', (req, res) => {
+//     const cpf = req.body.cpf
+//     pacientes.splice(pacientes.find((paciente,i)=>{if (paciente.cpf==cpf) return i}),1)
+//     res.status(204).end()
 // })
 
 module.exports = router;
