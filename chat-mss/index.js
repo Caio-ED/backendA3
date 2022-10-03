9e155e0d241c1e0524da5aae196e1b4a5ab849e5
Caio-ED <caioemidiodaniel@gmail.com>
2022-10-03 18:22:26 -0300

    feat: chat-mss começando microsserviço de chat para os usuarios

---
+++ chat-mss/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+const usuariosConectados = []
+const socketsConectados = []
+const mensagens = []
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on('on-line', (dados, resposta) => {
+
+        usuariosConectados.push(dados);
+        socketsConectados[dados.cpf] = socket;
+        resposta('Usuario Logado!')
+    })
+
+    socket.emit('historico-mensagens', mensagens);
+
+    socket.on('chat message', (msg) => {
+        mensagens.push(msg)
+        io.emit('chat message', msg);
+    });
+
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
