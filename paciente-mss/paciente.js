3cde5a4004cf0e9924b05e8e431d28fa9963de1b
catarinamanzieri <catarina.manzieri@gmail.com>
2022-10-10 07:20:21 -0300

    feat: paciente-mss - continuando rotas get e delete

--- paciente-mss/paciente.js
+++ paciente-mss/paciente.js
@@ -4,6 +4,10 @@ const router = express.Router();
 contador = 0;
 const pacientes = [];
 
+router.get('/pacientes', (req, res) => {
+    return res.json(pacientes); // só está exibindo a lista de pacientes na primeira requisição
+})
+
 router.post('/cadastro', (req, res) => {
     const {
         nome,
@@ -38,6 +42,16 @@ router.put('/alterarCadastro', (req, res) => {
     res.status(204).json(paciente);
 })
 
+router.get('/pesquisarPaciente', (req, res) => {
+    const cpf = req.query
+    const paciente = pacientes.find(pac =>{ if (pac.cpf == cpf) return pac})
+    res.status(200).json(paciente) // tá retornando OK mas não exibe o paciente no response
+})
 
+router.delete('/excluirPaciente', (req, res) => {
+    const cpf = req.body.cpf
+    pacientes.splice(pacientes.find((paciente,i)=>{if (paciente.cpf==cpf) return i}),1)
+    res.status(204).end()
+})
 
 module.exports = router;
